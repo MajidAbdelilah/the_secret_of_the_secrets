5e90a22a78c7d7f3f2841109660516d770bc208a
amajid <abdelilahmajid012@gmail.com>
2024-02-18 22:19:41 +0100

    adding the escape sequence block functionality and some optimization, note remove the use of memmove it consume a lote of time

--- example-3.c
+++ example-3.c
@@ -1,4 +1,6 @@
 #include <gtk/gtk.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -19,11 +21,11 @@ int count_untill_pipe(char *str)
 
 gsize adjust_quranic_text(char **text, gsize len)
 {
-	long long i = 0;
+	gsize i = 0;
 	while((*text)[i])
 	{
 		int skip_count = count_untill_pipe((*text) + i);
-		printf("skip = %d, i = %lld, len = %ld\n", skip_count, i, len);
+		printf("skip = %d, i = %ld, len = %ld\n", skip_count, i, len);
 		memmove((*text) + i, (*text) + i + skip_count, len - (i + skip_count));
 		while((*text)[i] && (*text)[i] != '\n')
 		{
@@ -37,9 +39,63 @@ gsize adjust_quranic_text(char **text, gsize len)
 	return i;
 }
 
-void remove_space_new_line(char **text, gsize len)
+gsize remove_space_new_line(char **text, gsize len)
 {
+	gsize i = 0;
+	while((*text)[i])
+	{
+		int j = 0;
+		while((*text)[i + j] == ' ')
+			j++;
+		if(j > 0)
+			memmove((*text) + i, (*text) + i + j, len - (i + j));
+		j = 0;
+		while((*text)[i + j] == '\n')
+			j++;
+		if(j > 0)
+			memmove((*text) + i, (*text) + i + j, len - (i + j));
+		printf("i = %ld\n", i);
+		i++;
+	}
+	(*text)[i] = 0;
+	return i;
+}
+
+gchar *escape_cequnce_block(char **text, gsize len, gsize escape, gsize block_size)
+{
+	gsize i = 0;
+	escape *= 2;
+	// block_size += 1;
+	block_size *= 2;
+	gchar *result = calloc(len, 1);
+	gsize res_len = 0;
 	
+	while(i < len && (*text)[i])
+	{
+		
+		if( !(i % block_size) )
+		{
+			result[res_len++] = ' ';
+			result[res_len++] = '\n';
+		}
+		gsize j = 0;
+		while(j < escape)
+			j++;
+		if((i + j) >= len)
+			j = len - i;
+		// memmove((*text) + i, (*text) + i + j, len - (i + j));
+		i += j;
+		
+		result[res_len++] = (*text)[i];
+		result[res_len++] = (*text)[i + 1];
+		char c[3] = {0};
+		c[0] = result[res_len - 2];
+		c[1] = result[res_len - 1];
+		printf("i = %ld, char = %s\n", i, c);
+		i += 2;
+	}
+	result = realloc(result, res_len + 4);
+	return result;
 }
 
 void activate_callback(GtkApplication *app, gpointer data)
@@ -70,10 +126,10 @@ void activate_callback(GtkApplication *app, gpointer data)
 	text_view = gtk_text_view_new();
 	text_buffet = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
 	gtk_text_buffer_set_text(text_buffet, text, -1);
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD);
+	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_NONE);
 	gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
 	gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(text_view), FALSE);
-
+	
 	
 	quran_file = g_file_new_for_path("./quran-simple-clean.txt");
 	
@@ -87,12 +143,16 @@ void activate_callback(GtkApplication *app, gpointer data)
       return;
     }
 	len = adjust_quranic_text(&content, len + 1);
-	gtk_text_buffer_set_text(text_buffet, content, len);
+	len = remove_space_new_line(&content, len + 1);
+	gchar *result = escape_cequnce_block(&content, len + 1, 10, 5);
+	printf("%s\n", result);
+	gtk_text_buffer_set_text(text_buffet, result, -1);
 	
 	scrool_window =  gtk_scrolled_window_new();
 	gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrool_window), text_view);
 
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrool_window), GTK_POLICY_ALWAYS, GTK_POLICY_ALWAYS);
+	gtk_scrolled_window_set_max_content_width(GTK_SCROLLED_WINDOW(scrool_window), INT_MAX);
 
 	gtk_window_set_child(GTK_WINDOW(window), scrool_window);
 
